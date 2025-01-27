8b75d1b7df97f5ad2f127a587902e44cd1c6882f
Ankit-2145 <ankitsharma2615@gmail.com>
2025-01-27 22:01:53 +0530

    add following cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+// import SplashCursor from "./components/bits/cursor";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      {/* <SplashCursor /> */}
       <Navbar />
       <Hero />
     </ThemeProvider>
