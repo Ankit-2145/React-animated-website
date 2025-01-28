d9571598d5771df02fd6042327e14c2144e66949
Ankit-2145 <ankitsharma2615@gmail.com>
2025-01-28 23:10:55 +0530

    add Hero section content

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,36 @@
 "use client";
 
-import DecryptedText from "./bits/decrypted-text";
+import { ShimmerButton } from "./magicui/shimmer-button";
+import { TextAnimate } from "./magicui/text-animate";
 
 export default function Hero() {
   return (
     <>
       <div className="mx-10 font-SpaceGrotesk">
-        <div className="max-w-5xl py-20">
-          <h1 className="text-5xl text-foreground tracking-wide leading-snug text-balance uppercase">
-            We don&apos;t just build softwares - we master what we do best,
-            every single time.
-          </h1>
-          <DecryptedText text="We are Pinnacle" />
+        <div className="py-20">
+          <div>
+            <h1 className="text-3xl md:text-5xl text-foreground tracking-wide text-balance uppercase">
+              <TextAnimate
+                className="pb-2"
+                animation="slideLeft"
+                by="character"
+              >
+                Crafting software,
+              </TextAnimate>
+              <TextAnimate animation="slideLeft" by="character">
+                perfected to the pixel
+              </TextAnimate>
+            </h1>
+            <p className="text-base py-5 text-muted-foreground">
+              Crafting digital solutions that scale, perform, and
+              inspire—tailored to your vision.
+            </p>
+            <ShimmerButton className="shadow-2xl">
+              <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+                Let's connect
+              </span>
+            </ShimmerButton>
+          </div>
         </div>
       </div>
     </>
